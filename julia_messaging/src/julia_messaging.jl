a27a1ff11ad8d9f86f734709ea47bcf4436faa52
ChiyenLee <johlee@g.hmc.edu>
2021-07-09 11:19:35 -0400

    moving something around

--- julia_messaging/src/julia_messaging.jl
+++ julia_messaging/src/julia_messaging.jl
@@ -3,7 +3,9 @@ module julia_messaging
 using ProtoBuf 
 using ZMQ 
 
-ProtoBuf.protoc(`-I=../ --julia_out=. message.proto`);
+# ProtoBuf.protoc(`-I=../ --julia_out=. message.proto`);
+dir_path = @__DIR__
+ProtoBuf.protoc(`-I=$dir_path/../../ --julia_out=. message.proto`);
 include("../message_pb.jl")
 include("messaging.jl")
 export create_pub, create_sub
