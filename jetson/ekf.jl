dd72122b44c78eb0d3e47164bf50996aeeaccb9d
ChiyenLee <johlee@g.hmc.edu>
2021-07-13 17:27:40 -0400

    something strange with quaternion update

--- jetson/ekf.jl
+++ jetson/ekf.jl
@@ -1,7 +1,7 @@
 using quadruped_control: prediction!
 using julia_messaging
 using quadruped_control
-
+using quadruped_control: UnitQuaternion, RotXYZ
 ## Subscribing example with ZMQ and Protobuf 
 function main()
     # Subscribe to Vicon topics
@@ -21,11 +21,17 @@ function main()
     u_imu = IMU_IN{Float64}()
     h = 0.05
     ekf = EKF(s)
-    v.R = v.R * 1e-3
+    v.R = v.R 
+    # s.W .= s.W * 1e3
+    # s.W .= s.W * 1e5
+    s.W[s.dβf.indices[1], s.dβf.indices[1]] .= s.W[s.dβf.indices[1], s.dβf.indices[1]] * 1e-2
+    s.W[s.dβω.indices[1], s.dβω.indices[1]] .= s.W[s.dβω.indices[1], s.dβω.indices[1]] * 1e-2
+
+    
 
     # timing 
     vicon_time = 0.0
-    h = 0.001
+    h = 0.01
     try
         while true 
             A1Robot.getAcceleration(interface, accel_imu);
@@ -39,18 +45,30 @@ function main()
             # Update 
             if hasproperty(vicon, :quaternion)
                if vicon.time != vicon_time 
-                    v.q .= [vicon.quaternion.z, vicon.quaternion.w, vicon.quaternion.x,
-                            vicon.quaternion.y]
+                    # v.q .= [vicon.quaternion.z, vicon.quaternion.w, vicon.quaternion.x,
+                            # vicon.quaternion.y]
+                    v.q .= [vicon.quaternion.w, vicon.quaternion.x, vicon.quaternion.y, vicon.quaternion.z]
                     v.r .= [vicon.position.x, vicon.position.y, vicon.position.z]
+                    println(v.q)
                     update!(ekf, v, h)
                     vicon_time = vicon.time
                end  
             end
 
-            # println(round.(ekf.est_state.q, digits=3))
+            println(round.(ekf.est_state.q, digits=3))
+            # println(round.(ekf.est_state.dϕ, digits=3))
             # println(round.(ekf.est_state.r, digits=3))
-            println(round.(ekf.est_state.v, digits=3))
+            # println(round.(ekf.est_state.v , digits=3))
+            # println(round.(ekf.est_state.βω, digits=3))
+            # println(ekf.est_state.βf)
+            # println(round.(accel_imu, digits=3))
             # println(ekf.est_state.q)
+            # q = ekf.est_state.q
+            # q = v.q
+            # g = [0, 0, 9.81]
+            # C = UnitQuaternion(q)
+            # println(round.(ekf.est_state.βf, digits=3))
+            # println(C' * accel_imu - g)
             sleep(h)
         end    
     catch e
