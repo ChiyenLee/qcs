43c6eb0d468b1c369a3dbabf710896ff09953bbc
ChiyenLee <johlee@g.hmc.edu>
2021-07-14 13:56:50 -0400

    wrote simple protobuf logger. need to modularize it

--- tools/load_data.py
+++ tools/load_data.py
@@ -1,12 +1,28 @@
+import sys 
+sys.path.append("..")
 import pymessaging.message_pb2 as msg
 import pdb
-imu = msg.IMU()
-vicon = msg.VICON()
-f = open("out.bin","rb")
-data = f.read(2)
-print(data)
-print(int.from_bytes(data, "big"))
-data = f.read(95)
-imu.ParseFromString(data)
-print(imu)
-f.close()
+
+# Initialize message 
+imu = msg.IMU_msg()
+vicon = msg.Vicon_msg()
+
+def read_messages(filename, proto_msg):
+	f = open(filename, "rb")
+	while True:
+		first_msg_length = f.read(4)
+
+		if len(first_msg_length) == 0:
+			break
+
+		first_msg_length = int.from_bytes(first_msg_length, "big")
+		data = f.read(first_msg_length)
+		proto_msg.ParseFromString(data)
+		print(proto_msg)
+		pdb.set_trace()
+		# print(first_msg_length)
+	
+	f.close()
+
+
+read_messages("imu.bin", imu)
--- tools/loggerd.py
+++ tools/loggerd.py
@@ -1,3 +1,58 @@
+import sys 
+sys.path.append("..")
 from pymessaging import messaging
-from pymessaging import message_pb2
+import pymessaging.message_pb2 as msg
+import zmq 
+import pdb
 
+def main():
+#     # Creating subscribers and poller
+    ctx = zmq.Context()
+    imu_sub = messaging.create_sub(ctx, 5001)
+    ekf_sub = messaging.create_sub(ctx, 5002)
+    vicon_sub = messaging.create_sub(ctx,5000)
+
+    poller = zmq.Poller()
+    poller.register(imu_sub, zmq.POLLIN)    
+    poller.register(ekf_sub, zmq.POLLIN)
+    poller.register(vicon_sub, zmq.POLLIN)
+
+    # Main loop for logging data 
+    imu_f = open("imu.bin", "wb")
+    vicon_f = open("vicon.bin", "wb")
+    ekf_f = open("ekf.bin", "wb")
+    try:
+        while True: 
+            socks = dict(poller.poll(0.01))
+
+            if imu_sub in socks.keys() and socks[imu_sub] == zmq.POLLIN:
+                # non blocking 
+                data = imu_sub.recv(zmq.DONTWAIT)
+                msg_size = len(data)
+                msg_size_b = (msg_size).to_bytes(4, byteorder="big")
+                imu_f.write(msg_size_b + data)
+            
+            if ekf_sub in socks.keys() and socks[ekf_sub] == zmq.POLLIN: 
+                data = ekf_sub.recv(zmq.DONTWAIT)
+                msg_size = len(data)
+                msg_size_b = (msg_size).to_bytes(4, byteorder="big")
+                ekf_f.write(msg_size_b + data)
+
+            if vicon_sub in socks.keys() and socks[vicon_sub] == zmq.POLLIN: 
+                data = vicon_sub.recv(zmq.DONTWAIT)
+                msg_size = len(data)
+                msg_size_b = (msg_size).to_bytes(4, byteorder="big")
+                vicon_f.write(msg_size_b + data)
+
+            
+
+    except KeyboardInterrupt:
+        imu_f.close()
+        vicon_f.close()
+        ekf_f.close()
+        print("interrupted!")
+
+
+
+if __name__ == "__main__":
+	main()
