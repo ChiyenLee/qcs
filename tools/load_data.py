5677adbe50de11a7b3e3f092b1c40cba2214ff0f
ChiyenLee <johlee@g.hmc.edu>
2021-07-27 15:56:19 -0400

    loading motor data

--- tools/load_data.py
+++ tools/load_data.py
@@ -31,11 +31,15 @@ class DataPack:
 imu = messaging.IMU_msg()
 ekf = messaging.EKF_msg()
 vicon = messaging.Vicon_msg()
+motor_msg = messaging.MotorReadings_msg()
+
 # file = os.path.join("A1data-2021-07-16--16-29-44", "imu.bin") # lots of turns and some lifting 
 # file = os.path.join("A1data-2021-07-16--16-22-05", "vicon.bin") # move around in the cage 
-file = os.path.join("A1data-2021-07-21--14-00-18", "ekf.bin")
+# file = os.path.join("A1data-2021-07-21--14-00-18", "ekf.bin")
+file = os.path.join("A1data-2021-07-26--10-54-48", "motor_state.bin")
 # bag = DataPack(file, vicon)
-bag = DataPack(file, ekf)
+# bag = DataPack(file, ekf)
+bag = DataPack(file, motor_msg)
 
 
 ang_bias = []
@@ -44,11 +48,13 @@ velocity = []
 position = []
 ang_velocity = []
 acc = []
+motor_pos = []
 for m in bag:
 	# ang_velocity.append([m.gyroscope.x, m.gyroscope.y, m.gyroscope.z])
 	# acc.append([m.acceleration.x, m.acceleration.y, m.acceleration.z])
-	position.append([m.position.x, m.position.y, m.position.z])
-	velocity.append([m.velocity.x, m.velocity.y, m.velocity.z])
+	# position.append([m.position.x, m.position.y, m.position.z])
+	# velocity.append([m.velocity.x, m.velocity.y, m.velocity.z])
+	motor_pos.append([m.q.FR_Hip, m.q.FR_Thigh])
 ang_velocity = np.array(ang_velocity)
 acc = np.array(acc)
 position = np.array(position)
